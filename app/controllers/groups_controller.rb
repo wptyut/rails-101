18b97d44f3e91e473e7b532150a6e62b8f594a7b
wptyut <wptyut@163.com>
2017-04-02 18:01:00 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,8 +26,14 @@ class GroupsController < ApplicationController
     @group = Group.update(group_params)
 
     redirect_to groups_path, notice: "Update Success"
-    end
+  end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
   private
 
   def group_params
