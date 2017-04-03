0c286034f5ae97a4b5abb57347f4ca778e22877c
wptyut <wptyut@163.com>
2017-04-03 11:08:26 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,9 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
+
 
   def edit
 
@@ -37,7 +39,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    
+
     @group.destroy
       redirect_to groups_path, alert: "Group deleted"
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :group
+  belongs_to :
+  validates :content, presence: true
+
 end
